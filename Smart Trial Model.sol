3347e45279d52b171eb4aeb8ba36f03d7a509edd
Gozie01 <cnnadiekwe01@gmail.com>
2024-08-21 17:08:59 +0900

    Update Smart Trial Model.sol

--- Smart Trial Model.sol
+++ Smart Trial Model.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.10;
 
-contract SmartTrialModel{
+contract colabresearch{
 
     struct Org{
         mapping(address => Member) members;
@@ -26,14 +26,35 @@ contract SmartTrialModel{
         uint [] nextAP;
         string [] labResult;
         uint [] labResultTime;
+        uint balance;
+        uint userAppointmentCount;
+
     }
+
+    uint appointmentCount;
+    uint interval;
+
 
     string patient = "patient";
     string clinicalTeam = "clinicalTeam";
+    address  payable public patients;
+    address public owner;
 
     // address public admin;
     mapping(string => Org) orgs;
     mapping (address => healthData) healthDatas;
+
+
+        function withdraw(address _patient) public {
+        uint256 amount = healthDatas[_patient].balance;
+        require(amount > 0, "No balance to withdraw");
+        healthDatas[_patient].balance = 0;
+        payable(_patient).transfer(amount);
+        }
+
+        function deposit()external  payable {
+
+        }
 
 
     function registerMember(address _memberAddress, string memory _name, string memory _org, string memory _role, string memory _profile) public {
@@ -51,10 +72,16 @@ contract SmartTrialModel{
 
 
     function updateHealthStat(address _userAdd, string memory _healthStat)external{
+        if (appointmentCount >= block.timestamp - interval ){
+            appointmentCount = appointmentCount + interval;
+        }
+        
         require(orgs[patient].members[_userAdd].isRegistered == true);
         require(orgs[clinicalTeam].members[msg.sender].isRegistered == true);
+        require(healthDatas[_userAdd].userAppointmentCount >= appointmentCount - 2*interval);
         healthDatas[_userAdd].healthStat.push(_healthStat);
         healthDatas[_userAdd].healthStatTime.push(block.timestamp);
+        healthDatas[_userAdd].balance = healthDatas[_userAdd].balance + 250000000000000000;
         
         
     }
@@ -64,7 +91,7 @@ contract SmartTrialModel{
         require(orgs[clinicalTeam].members[msg.sender].isRegistered == true);
         healthDatas[_userAdd].labResult.push(_labResult);
         healthDatas[_userAdd].labResultTime.push(block.timestamp);
-        
+        healthDatas[_userAdd].balance = healthDatas[_userAdd].balance + 250000000000000000;
         
     }
 
@@ -74,6 +101,7 @@ contract SmartTrialModel{
         healthDatas[_userAdd].prescription.push(_prescription);
         healthDatas[_userAdd].dosage.push(_dosage);
         healthDatas[_userAdd].prescriptionTime.push(block.timestamp);
+        healthDatas[_userAdd].balance = healthDatas[_userAdd].balance + 250000000000000000;
                
     }
         function updatenextAP(address _userAdd, uint _nextAP)public {
@@ -90,4 +118,5 @@ contract SmartTrialModel{
 
 
 }
+
 }
